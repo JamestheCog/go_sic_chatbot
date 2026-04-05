9a28042b21559cbd1b78dabcb3158c2d2b311ca8
JamestheCog <jcog196013@gmail.com>
2026-04-05 23:09:08 +0800

    task: changed body definition of IIFE

--- static/js/shared/theme_toggler.js
+++ static/js/shared/theme_toggler.js
@@ -1,7 +1,13 @@
 // NOTE: 'nightModeOn' comes from /static/js/chat/theme.js - but it's just moved here 
 //       for the sake of convenience...
 (function() {
-    let hour = new Date().getHours();
-    let shouldBeDark = hour < 7 || hour > 19
-    if (shouldBeDark) document.querySelector('body').classList.add('dark');
+    let userPref = localStorage.getItem('nightModeOn');
+    
+    if (!userPref) {
+        let hour = new Date().getHours();
+        let shouldBeDark = hour < 7 || hour > 19
+        if (shouldBeDark) document.querySelector('body').classList.add('dark');
+    } else {
+        document.querySelector('body').classList.toggle('dark', userPref === 'true');
+    }
 })();
