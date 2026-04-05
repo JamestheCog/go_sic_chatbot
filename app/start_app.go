f112f64e5dc7d1a57bacd154f99afe1fd29a8ff6
JamestheCog <jcog196013@gmail.com>
2026-04-05 18:00:29 +0800

    task: removed unnecessary constants from InitializeApp()

--- app/start_app.go
+++ app/start_app.go
@@ -5,73 +5,88 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"strings"
 
 	"github.com/gorilla/sessions"
+	"github.com/jamesthecog/go_chatbot/utils"
 	sqlitecloud "github.com/sqlitecloud/sqlitecloud-go"
-	"google.golang.org/genai"
 )
 
 // The App object that contains important objects needed
 // for the bot's proper functioning.  Also what InitializeApp() will
 // return.
 type App struct {
-	GeminiClient  *genai.Client
-	DbClient      *sqlitecloud.SQCloud
-	AesKey        string
-	ContentConfig *genai.GenerateContentConfig
-	Cookie        *sessions.CookieStore
+	DbClient           *sqlitecloud.SQCloud
+	AesKey             string
+	GeminiKeys         []string
+	Cookie             *sessions.CookieStore
+	ConversationPrompt string
 }
 
 // Constants necessary for the proper functioning of the bot:
 const (
-	promptPath = "./resources/base_bot_prompt.txt"
+	promptPath    = "./resources/base_bot_prompt.txt"
+	geminiKeyPath = "./resources/gemini_keys.txt"
 )
 
 // The function of interest that'll allow us to set up our application's struct -
 // it mainly ensures that our Gemini client and dbConnection strings are in place.
 func InitializeApp(ctx context.Context) (*App, error) {
 	app := &App{}
-	geminiKey := os.Getenv("GEMINI_KEY")
-	dbConnString := os.Getenv("SQLITECLOUD_CONNECTION_KEY")
+	dbConnString := os.Getenv("SQLITECLOUD_CONNECTION_STRING")
 	aesKey := os.Getenv("AES_KEY")
 	appSecret := os.Getenv("APP_SECRET")
-	if geminiKey == "" {
-		return nil, fmt.Errorf("null value for `geminiKey`.")
-	}
 	if dbConnString == "" {
-		return nil, fmt.Errorf("null value for `dbConnString`.")
+		return nil, fmt.Errorf("null value for `SQLITECLOUD_CONNECTION_STRING`.")
 	}
 	if aesKey == "" {
-		return nil, fmt.Errorf("null value for `aesKey`.")
+		return nil, fmt.Errorf("null value for `AES_KEY`.")
 	}
 	if appSecret == "" {
-		return nil, fmt.Errorf("null value for `appSecret`.")
+		return nil, fmt.Errorf("null value for `APP_SECRET`.")
 	}
 
-	// Create what needs to be created here:
-	geminiClient, err := genai.NewClient(ctx, &genai.ClientConfig{APIKey: geminiKey})
-	if err != nil {
-		return nil, err
-	}
+	app.AesKey = aesKey
 	dbClient, err := sqlitecloud.Connect(dbConnString)
 	if err != nil {
 		dbClient.Close()
 		return nil, err
 	}
-	app.AesKey = aesKey
-	contentConfig, err := app.createContentConfig(promptPath)
+	conversationPrompt, err := utils.LoadFile(promptPath, aesKey)
+	if err != nil {
+		return nil, err
+	}
+	geminiKeys, err := fetchGeminiKeys(geminiKeyPath, aesKey)
 	if err != nil {
 		return nil, err
 	}
 	cookieStore := sessions.NewCookieStore([]byte(appSecret))
 
 	// Assign values to the app object here:
-	app.GeminiClient = geminiClient
 	app.DbClient = dbClient
-	app.ContentConfig = contentConfig
+	app.ConversationPrompt = conversationPrompt
 	app.Cookie = cookieStore
-
+	app.GeminiKeys = geminiKeys
 	return app, nil
+}
+
+// Given a file path and a hex key, return the file's decrypted contents
+// as a string:
+
+// Given a the file path to the Gemini keys, return a string slice containing its
+// contents:
+func fetchGeminiKeys(filePath, hexKey string) ([]string, error) {
+	decryptedContents, err := utils.LoadFile(filePath, hexKey)
+	if err != nil {
+		return nil, err
+	}
+
+	tokens := strings.Split(strings.TrimSpace(decryptedContents), "\n")
+	toReturn := make([]string, len(tokens))
+	for i, v := range tokens {
+		toReturn[i] = strings.TrimSpace(v)
+	}
+	return toReturn, nil
 }
 
 // --- Struct-related methods ---
@@ -80,6 +95,7 @@ func InitializeApp(ctx context.Context) (*App, error) {
 // both internal and otherwise:
 func (a *App) RegisterRoutes() {
 	fs := http.FileServer(http.Dir("./static"))
+	http.Handle("/favicon.ico", http.HandlerFunc(a.faviconHandler))
 	http.Handle("/static/", http.StripPrefix("/static/", fs))
 	http.Handle("/manifest.json", http.HandlerFunc(a.serveManifest))
 	http.Handle("/sw.js", http.HandlerFunc(a.serveServiceWorker))
@@ -89,6 +105,7 @@ func (a *App) RegisterRoutes() {
 	http.Handle("/signup", http.HandlerFunc(a.creationPageHandler))
 	http.Handle("/login", http.HandlerFunc(a.loginPageHandler))
 	http.Handle("/chat", a.sessionPropagation(http.HandlerFunc(a.chatPageHandler)))
+	http.Handle("/offline", http.HandlerFunc(a.offlinePageHandler))
 
 	// Internal routes:
 	//
